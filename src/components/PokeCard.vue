2bd9c28f465f0e54ceb8c5e757391681b154b489
sollendy <edlannino@tutanota.com>
2024-06-10 10:41:27 +0200

    inserire stats in pokecard vedi pseudocodice

--- src/components/PokeCard.vue
+++ src/components/PokeCard.vue
@@ -19,26 +19,27 @@ export default {
 
 <template>
     <div class="foto-stats h-100 d-flex flex-column gap-3">
-        <div class="foto-cont w-75 bg-secondary p-2 d-flex justify-content-center align-self-center ms-5 me-5">
-            <img v-if="pokemon !== ''" class="w-100 border border-4 border-black" :src="pokemon.sprites.front_default" alt="pokéfoto">
-            <img v-else class="w-100 border border-4 border-black p-3" src="https://www.pngall.com/wp-content/uploads/4/Pokeball-PNG-Images.png" alt="pokéfoto"> 
+        <div class="foto-cont w-75 border border-4 border-black bg-light p-2 d-flex justify-content-center align-self-center ms-5 me-5">
+            <img v-if="pokemon !== ''" class="w-50" :src="pokemon.sprites.front_default" alt="pokéfoto">
+            <img v-else class="w-50 p-3" src="https://www.pngall.com/wp-content/uploads/4/Pokeball-PNG-Images.png" alt="pokéfoto"> 
 
         </div>
         <div class="stats-cont h-100">
-            <div class="stats border border-dark rounded h-100 w-100">
-                <ul class="prime-info" v-if="pokemon !== ''">
-                    <li>Nome: <b> {{ pokemon.name}} </b></li>
-                    <li>Tipo: <b> {{ pokemon.types[0].type.name}} </b></li>
-                    <li>Altezza: <b> {{ pokemon.height}} </b></li>
-                    <li>Peso: <b> {{ pokemon.Weight}} </b></li>
+            <div v-if="pokemon !== ''"  class="stats border border-dark rounded h-100 w-100">
+                <ul class="info prime-info">
+                    <li>Nome: <b> {{ pokemon.name}} </b>;</li>
+                    <li>Tipo: <b v-for="(tipi, index) in pokemon.types" :key="index"> {{ tipi.type.name}}, </b>;</li>
+                    <li>Altezza: <b> {{ pokemon.height}}"</b>;</li>
+                    <li>Peso: <b> {{ pokemon.weight}}lbs</b>;</li>
                 </ul>
-                 <ul class="prime-info" v-else>
-                    Nessun Pokémon valido scelto.
+                <ul class="info second-info">
+                    <!-- Qui devo inserire una barra piena in base a quanto è alta
+                     ciascuna delle caratteristiche relative al pokémon cercato -->
                 </ul>
             </div>
-            <!-- vorrei che tutto ciò che è visibile dentro le ul sia visibile SOLO quando viene cercato il pokémon,
-            pertanto non so perché il testo "name:" non viene nascosto dato che io nel v-if in teoria,
-            sto dicendo che il contenuto di ul deve essere visibile SOLO se la props pokemon è diversa da niente. -->
+            <div v-else class="stats text-center border border-dark rounded pt-2 h-100 w-100">
+                Nessun Pokémon valido scelto.
+            </div>
         </div>
     </div>
 </template>
@@ -49,7 +50,7 @@ export default {
         .stats {
             background-color: rgb(43, 212, 12);
             width: 90%;
-            .prime-info {
+            .info {
                 list-style-type: none;
             }
         }
